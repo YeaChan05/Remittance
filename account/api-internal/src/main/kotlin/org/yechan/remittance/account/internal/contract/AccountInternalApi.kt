56deabd8e7f01643a03d9710e84b02e0b6834a30
qkenrdl05 <qkenrdl05@gmail.com>
2026-04-08 14:12:24 +0900

    refactor(account-api-internal): 요청 헤더 상수화 및 내부 요청 헤더 재사용

--- account/api-internal/src/main/kotlin/org/yechan/remittance/account/internal/contract/AccountInternalApi.kt
+++ account/api-internal/src/main/kotlin/org/yechan/remittance/account/internal/contract/AccountInternalApi.kt
@@ -4,26 +4,25 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.service.annotation.HttpExchange
 import org.springframework.web.service.annotation.PostExchange
-
-private const val INTERNAL_USER_ID_HEADER = "X-Internal-User-Id"
+import org.yechan.remittance.InternalRequestHeaders
 
 @HttpExchange("/internal/accounts")
 interface AccountInternalApi {
     @PostExchange("/query")
     fun get(
-        @RequestHeader(INTERNAL_USER_ID_HEADER) memberId: Long,
+        @RequestHeader(InternalRequestHeaders.USER_ID) memberId: Long,
         @RequestBody request: AccountGetRequest,
     ): AccountSnapshotResponse?
 
     @PostExchange("/lock")
     fun lock(
-        @RequestHeader(INTERNAL_USER_ID_HEADER) memberId: Long,
+        @RequestHeader(InternalRequestHeaders.USER_ID) memberId: Long,
         @RequestBody request: AccountLockRequest,
     ): AccountLockResponse?
 
     @PostExchange("/balance-change")
     fun applyBalanceChange(
-        @RequestHeader(INTERNAL_USER_ID_HEADER) memberId: Long,
+        @RequestHeader(InternalRequestHeaders.USER_ID) memberId: Long,
         @RequestBody request: AccountBalanceChangeRequest,
     ): AccountBalanceChangeResponse
 }
---
+++ common/api/src/main/kotlin/org/yechan/remittance/InternalRequestHeaders.kt
@@ -0,0 +1,6 @@
+package org.yechan.remittance
+
+object InternalRequestHeaders {
+    const val TOKEN = "X-Internal-Token"
+    const val USER_ID = "X-Internal-User-Id"
+}
