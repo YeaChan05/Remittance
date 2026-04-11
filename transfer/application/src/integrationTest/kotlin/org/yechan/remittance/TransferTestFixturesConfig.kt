2e553311ba7a523e88d88f311dca07085795882d
qkenrdl05 <qkenrdl05@gmail.com>
2026-04-11 14:20:58 +0900

    test(remittance): 실패 시뮬레이션 및 대체 리포지토리 설정 추가

--- transfer/application/src/integrationTest/kotlin/org/yechan/remittance/TransferTestFixturesConfig.kt
+++ transfer/application/src/integrationTest/kotlin/org/yechan/remittance/TransferTestFixturesConfig.kt
@@ -4,18 +4,28 @@ import jakarta.persistence.EntityManager
 import org.springframework.beans.factory.config.BeanPostProcessor
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
-import org.springframework.test.web.servlet.client.RestTestClient
+import org.springframework.context.annotation.Primary
 import org.springframework.transaction.support.TransactionSynchronization
 import org.springframework.transaction.support.TransactionSynchronizationManager
 import org.springframework.transaction.support.TransactionTemplate
 import org.yechan.remittance.transfer.TransferAccountClient
+import org.yechan.remittance.transfer.TransferAccountIdentifier
 import org.yechan.remittance.transfer.TransferAccountLockCommand
 import org.yechan.remittance.transfer.TransferAccountSnapshot
 import org.yechan.remittance.transfer.TransferBalanceChangeCommand
+import org.yechan.remittance.transfer.TransferIdentifier
 import org.yechan.remittance.transfer.TransferLockedAccounts
+import org.yechan.remittance.transfer.TransferModel
+import org.yechan.remittance.transfer.TransferProps
+import org.yechan.remittance.transfer.TransferQueryCondition
+import org.yechan.remittance.transfer.TransferRepository
+import org.yechan.remittance.transfer.TransferRequestProps
 import org.yechan.remittance.transfer.config.TransferApplicationAccountStore
 import org.yechan.remittance.transfer.config.TransferApplicationMemberStore
 import org.yechan.remittance.transfer.config.TransferInternalApiStubEnvironment
+import java.math.BigDecimal
+import java.time.LocalDateTime
+import java.util.concurrent.atomic.AtomicBoolean
 
 @TestConfiguration
 class TransferTestFixturesConfig {
@@ -27,7 +37,6 @@ class TransferTestFixturesConfig {
 
     @Bean
     fun transferTestFixtures(
-        restTestClient: RestTestClient,
         em: EntityManager,
         transactionTemplate: TransactionTemplate,
         tokenGenerator: TokenGenerator,
@@ -55,6 +64,16 @@ class TransferTestFixturesConfig {
             return CommitAwareTransferAccountClient(bean)
         }
     }
+
+    @Bean
+    fun transferFailureSwitch(): TransferFailureSwitch = TransferFailureSwitch()
+
+    @Bean
+    @Primary
+    fun failureTransferRepository(
+        delegate: TransferRepository,
+        transferFailureSwitch: TransferFailureSwitch,
+    ): TransferRepository = FailureTransferRepository(delegate, transferFailureSwitch)
 }
 
 private class CommitAwareTransferAccountClient(
@@ -80,4 +99,44 @@ private class CommitAwareTransferAccountClient(
         }
         delegate.applyBalanceChange(command)
     }
+}
+
+class TransferFailureSwitch {
+    private val enabled = AtomicBoolean(false)
+
+    fun enable() {
+        enabled.set(true)
+    }
+
+    fun disable() {
+        enabled.set(false)
+    }
+
+    fun shouldFail(): Boolean = enabled.get()
+}
+
+private class FailureTransferRepository(
+    private val delegate: TransferRepository,
+    private val failureSwitch: TransferFailureSwitch,
+) : TransferRepository {
+    override fun save(props: TransferRequestProps): TransferModel {
+        if (failureSwitch.shouldFail()) {
+            throw IllegalStateException("Transfer save failed")
+        }
+        return delegate.save(props)
+    }
+
+    override fun findById(identifier: TransferIdentifier): TransferModel? = delegate.findById(identifier)
+
+    override fun findCompletedByAccountId(
+        identifier: TransferAccountIdentifier,
+        condition: TransferQueryCondition,
+    ): List<TransferModel> = delegate.findCompletedByAccountId(identifier, condition)
+
+    override fun sumAmountByFromAccountIdAndScopeBetween(
+        identifier: TransferAccountIdentifier,
+        scope: TransferProps.TransferScopeValue,
+        from: LocalDateTime,
+        to: LocalDateTime,
+    ): BigDecimal = delegate.sumAmountByFromAccountIdAndScopeBetween(identifier, scope, from, to)
 }
