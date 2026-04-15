9a711b5e662f55c3a7db942f82eef6fa6659cce8
qkenrdl05 <qkenrdl05@gmail.com>
2026-04-16 08:57:30 +0900

    refactor(remittance): RestTestClient 설정에서 ApiVersionInserter 제거 및 설정 간소화

--- common/application-api/src/testFixtures/kotlin/org/yechan/remittance/RestTestClientTestConfiguration.kt
+++ common/application-api/src/testFixtures/kotlin/org/yechan/remittance/RestTestClientTestConfiguration.kt
@@ -4,7 +4,6 @@ import org.springframework.boot.autoconfigure.AutoConfiguration
 import org.springframework.context.annotation.Bean
 import org.springframework.test.web.servlet.client.RestTestClient
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
-import org.springframework.web.client.ApiVersionInserter
 import org.springframework.web.context.WebApplicationContext
 
 @AutoConfiguration
@@ -13,7 +12,6 @@ class RestTestClientTestConfiguration {
     fun restTestClient(context: WebApplicationContext): RestTestClient {
         val mockMvc = MockMvcBuilders.webAppContextSetup(context).build()
         return RestTestClient.bindTo(mockMvc)
-            .apiVersionInserter(ApiVersionInserter.useHeader("X-API-Version"))
             .defaultHeader("X-API-Version", "v1")
             .build()
     }
